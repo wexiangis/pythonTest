3fc8c5259a818e0488e3eeed216adc50d38796ba
WWX <593805475@qq.com>
2018-09-12 19:46:56 +0800

    print and string test

--- basebase/print_test.py
+++ basebase/print_test.py
@@ -1,16 +1,31 @@
+#!/usr/bin/python3.4
 
 pInt = 123
 pFloat = 123.456
 pBool = True
 pArray = 1,2,3
+pString='1234567890\n123'
 
-#coding=utf-8
+#-*-coding:utf-8-*-
 '''
+
 print方法的使用:
-    print(*objects,sep="",end="\n",file=sys.stdout,flush=False)
+
+print(*objects,sep="",end="\n",file=sys.stdout,flush=False)
+
 '''
 print(pInt,pFloat,pBool,pArray)
 print(pInt,pFloat,pBool,pArray,sep="|",end=" this end\r\n")
 
-name=input("your name is ")
-print("XXXOOO -> ", name)
+print("----- string test -----")
+print(pString[0])
+print(pString[-2])
+print(pString[3:5])
+print(pString[5:])
+print(pString*2)
+print("----- string test end -----")
+
+"this is a \N{dog}"
+
+# name=input("your name is ")
+# print("XXXOOO -> ", name)
---
+++ else/turtle_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.4
+
+from turtle import *
+
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
